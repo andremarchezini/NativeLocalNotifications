2778a8d599977e3149e7945a1ee466257773cda2
Andre <andremarchezini@gmail.com>
2020-12-23 11:33:57 +0800

    added a notification that is send when button is clicked

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -1,11 +1,19 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -23,11 +31,31 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                Log.i("Info", "Schedule clicked");
+                String CHANNEL_ID = "CH1";
+                String CHANNEL_NAME = "CHANNEL";
+                try {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        @SuppressLint("WrongConstant") NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_MAX);
+                        NotificationManager notificationManager = MyApplication.getAppContext().getSystemService(NotificationManager.class);
+                        notificationManager.createNotificationChannel(channel);
+                    }
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(MyApplication.getAppContext(), CHANNEL_ID)
+                            .setSmallIcon(R.drawable.ic_stat_name)
+                            .setContentTitle("My notification")
+                            .setContentText("This is a notification")
+                            .setStyle(new NotificationCompat.BigTextStyle()
+                                    .bigText("This is a notification. Much longer text that cannot fit one line..."))
+                            .setPriority(NotificationCompat.PRIORITY_MAX);
+
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MyApplication.getAppContext());
+                    notificationManager.notify(1, builder.build());
+                } catch (Error e){
+                    Log.i("Error", e.toString());
+                }
             }
         });
     }
